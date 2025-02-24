3a531a096cd0233905439768ce11b16361f76262
chaitaitanya <chaitanyavijayvidhate00@gmail.com>
2025-02-24 20:23:09 +0530

    Add files via upload

---
+++ portf.js
@@ -0,0 +1,6 @@
+let navToggle=document.getElementById("nav-toggle")
+let navBox=document.getElementById("nav-box")
+
+navToggle.addEventListener("click",()=>{
+    navBox.classList.toggle('active')
+})
